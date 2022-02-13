aa2b20aed211616a51a0999f72a6c5c755c18ed8
konradmilewski <konrad@kmilewski.eu>
2022-02-13 17:39:45 +0100

    initial

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'debug' => false,
+  'database' =>
+  array (
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'port' => 3309,
+    'database' => 'flarum',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => NULL,
+    'prefix_indexes' => true,
+  ),
+  'url' => 'https://dbr_flarum.test',
+  'paths' =>
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' =>
+  array (
+    'poweredByHeader' => false,
+    'referrerPolicy' => 'same-origin',
+  ),
+);
---
+++ storage/views/83bc0c305d07622e4ff0116fd04d05b44dc96c3c.php
@@ -0,0 +1,2 @@
+<img src="{@url}" title="{@base_name}" alt="{@base_name}"/>
+<?php /**PATH /Users/konradmilewski/Projects/APW Projects/dbr_flarum/vendor/fof/upload/resources/templates/image-preview.blade.php ENDPATH**/ ?>
